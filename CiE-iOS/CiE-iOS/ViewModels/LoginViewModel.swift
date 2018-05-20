846846df93755be94dea1e2db8ad1e419198d277
jackty <jack_ty@hotmail.com>
2018-05-20 18:44:51 +0200

    created Log in Page
    Dummy input text field for username and password, click on "Log in" button to proceed to main page - courses view

---
+++ CiE-iOS/CiE-iOS/ViewModels/LoginViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewModel.swift
+//  CiE-iOS
+//
+//  Created by Jack Tan on 20/5/18.
+//  Copyright © 2018 Nikita Hans. All rights reserved.
+//
+
+protocol LoginViewModelProtocol {
+    
+}
+
+class LoginViewModel: LoginViewModelProtocol {
+    
+}
