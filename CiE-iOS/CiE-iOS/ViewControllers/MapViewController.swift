2e4e1e92826e046c43b554792315093f65dc51f9
eamlilli <37478992+eamlilli@users.noreply.github.com>
2018-05-25 00:20:31 +0200

    Features/27 implement skeleton of map view (#55)
    * Maptable created;
    
    * 27 - skeleton_MapView
    added the three campus maps;
    implemented a UITapGestureRecognizer for onClick-Action (later: shows map in fullscreen)
    
    * Maptable created;
    
    * 27 - skeleton_MapView
    added the three campus maps;
    implemented a UITapGestureRecognizer for onClick-Action (later: shows map in fullscreen)
    
    * 27-Implement-skeleton-of-MapView
    added descriptions (Campus und so weiter...)

--- CiE-iOS/CiE-iOS/ViewControllers/MapViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/MapViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class MapViewController: UIViewController {
     
+    public private(set) var lastTappedImage: UIImageView?
+    
+    @IBOutlet weak var lothstr: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        lothstr.isUserInteractionEnabled = true
+        lothstr.addGestureRecognizer(tapGestureRecognizer)
+        dropShadow(on: lothstr)
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    private func dropShadow(on picture: UIImageView) {
+        picture.layer.shadowOffset = CGSize(width: 0, height: 3)
+        picture.layer.shadowOpacity = 0.8
+        picture.layer.shadowRadius = 3.0
+        picture.layer.shadowColor = UIColor.black.cgColor
+    }
+    
+    @objc
+    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer){
+        lastTappedImage = tapGestureRecognizer.view as? UIImageView
+        print("Tapped on Lothstr.")
+    }
+    
+    
 }
 
