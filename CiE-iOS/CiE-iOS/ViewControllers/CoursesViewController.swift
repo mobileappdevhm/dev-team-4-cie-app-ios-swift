98868581ac4bb011d8d7322190cdba2e84c27bd2
Snezana Eckl <37689789+Sneckl@users.noreply.github.com>
2018-05-28 17:45:10 +0200

    26 Added TimeTable and navigation bar to courses view controller (#61)

--- CiE-iOS/CiE-iOS/ViewControllers/CoursesViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/CoursesViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
-class CoursesViewController: UIViewController {
+class CoursesViewController: UIViewController{
+
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //tableView.separatorColor = 
+        //navBar.topItem?.title = "Course List"
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+
 }
 
