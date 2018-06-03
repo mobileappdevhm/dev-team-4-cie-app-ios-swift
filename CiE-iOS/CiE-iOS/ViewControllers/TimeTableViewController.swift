594c424f934d338ab64c556ef807bfec2718217b
jackty <jack_ty@hotmail.com>
2018-06-04 00:15:58 +0200

    added UIDatePicker

--- CiE-iOS/CiE-iOS/ViewControllers/TimeTableViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/TimeTableViewController.swift
@@ -9,20 +9,45 @@
 import Foundation
 import UIKit
 
-class TimetableViewController: UIViewController {
+class TimeTableViewController: UIViewController {
     
     @IBOutlet weak var dateField: UITextField!
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    let picker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       createDatePicker()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
 
+    func createDatePicker(){
+        
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //done button for toolbar
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([done], animated: false)
+        
+        dateField.inputAccessoryView = toolbar
+        dateField.inputView = picker
+        
+        //format picker for date
+        picker.datePickerMode = .date
+    }
     
+    @objc func donePressed(){
+        //format date
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        formatter.timeStyle = .none
+        
+        let dateString = formatter.string(from: picker.date)
+        
+        dateField.text = "\(dateString)"
+        self.view.endEditing(true)
+    }
 }
 
