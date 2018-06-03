ef228a93c2d4939ef280d269d7524e92797d05cd
jackty <jack_ty@hotmail.com>
2018-06-03 21:29:59 +0200

    added UISearchController

--- CiE-iOS/CiE-iOS/ViewControllers/CoursesViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/CoursesViewController.swift
@@ -16,9 +16,8 @@ class CoursesViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //tableView.separatorColor = 
-        //navBar.topItem?.title = "Course List"
-        
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
     }
     
     override func didReceiveMemoryWarning() {
