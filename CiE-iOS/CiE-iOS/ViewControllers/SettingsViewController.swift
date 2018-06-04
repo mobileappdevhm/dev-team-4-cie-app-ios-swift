61fd3a7a9ca3396150c6b31f27e22c766b991777
nikitahans <nikita.s.hans@googlemail.com>
2018-06-05 01:33:57 +0200

    origin/feature/47-Login-Screen-Vie removing logout button

--- CiE-iOS/CiE-iOS/ViewControllers/SettingsViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/SettingsViewController.swift
@@ -9,22 +9,9 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-    
-    @IBOutlet weak var logOut: UIButton!
-    
-    @IBAction func logoutButtonPressed(_ sender: Any) {
-         self.performSegue(withIdentifier: "Login View Controller", sender: self)
-    }
-    
-    private func setupstyling(){
-        logOut.setTitle("Heyho", for: .normal)
-    }
-    
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        setupstyling()
     }
     
     override func didReceiveMemoryWarning() {
