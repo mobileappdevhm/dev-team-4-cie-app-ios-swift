6aee7ceab27079be7888f216af76e982d3b7d3d2
jackty <jack_ty@hotmail.com>
2018-06-13 23:52:02 +0200

    added UI date picker

--- CiE-iOS/CiE-iOS/ViewControllers/TimeTableViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/TimeTableViewController.swift
@@ -9,15 +9,49 @@
 import UIKit
 
 class TimeTableViewController: UIViewController {
+
+    @IBOutlet weak var dateField: UITextField!
+    
+    let picker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createDatePicker()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //date selection feature
+    func createDatePicker(){
+        
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //done button for toolbar
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([done], animated: false)
+        
+        dateField.inputAccessoryView = toolbar
+        dateField.inputView = picker
+        
+        //format picker for date
+        picker.datePickerMode = .date
+    }
+    
+    @objc func donePressed(){
+        //format date
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        formatter.timeStyle = .none
+        
+        let dateString = formatter.string(from: picker.date)
+        
+        dateField.text = "\(dateString)"
+        self.view.endEditing(true)
     }
 }
 
