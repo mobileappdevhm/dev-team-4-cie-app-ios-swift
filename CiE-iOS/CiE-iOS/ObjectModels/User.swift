5564e067ff8fe7aabb03bb3a50e0d70e8a55bd2a
nikitahans <nikita.s.hans@googlemail.com>
2018-06-14 14:46:49 +0200

    31-SettingsView Creating scrollable settingsView with connection to user Service Settings page not working due to lacking backend connection Stats working fixing all warnings out of main storyboard NOTE: Try to understand storyboard via identifications the visual representation is misleading

--- CiE-iOS/CiE-iOS/ObjectModels/User.swift
+++ CiE-iOS/CiE-iOS/ObjectModels/User.swift
@@ -7,11 +7,12 @@
 //
 
 protocol UserProtocol {
-    init(withName name: String, andPassword: String)
+    init(withName name: String, andPassword: String, usingEmail: String)
     var name:String { get }
     var password: String { get }
     var isExchange: Bool? { get }
     var departments: Set<Department> { get }
+    var email: String { get }
     
     func changePassword(to: String)
     func becomeExchangeStudent()
@@ -24,12 +25,14 @@ class User: UserProtocol {
     public private(set) var name: String
     public private(set) var password: String
     public private(set) var isExchange: Bool? = false
+    public private(set) var email: String
     
     public private(set) var departments: Set<Department> = []
     
-    required init(withName name: String, andPassword password: String) {
+    required init(withName name: String, andPassword password: String, usingEmail email: String) {
         self.name = name
         self.password = password
+        self.email = email
     }
 
     func changePassword(to newPassword: String) {
--- CiE-iOS/CiE-iOS/ViewControllers/SettingsViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/SettingsViewController.swift
@@ -10,18 +10,127 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var userIconImageView: UIImageView!
+    @IBOutlet weak var emailImageView: UIImageView!
+    
+    @IBOutlet weak var statsButton: UIButton!
+    @IBOutlet weak var statsStack: UIStackView!
+    @IBOutlet weak var currentECTS: UILabel!
+    @IBOutlet weak var currentECTSProgress: UIProgressView!
+    
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var settingStack: UIStackView!
+    @IBOutlet weak var currentCiE: UILabel!
+    @IBOutlet weak var cieCertificateInfo: UILabel!
+    @IBOutlet weak var currentCiEProgress: UIProgressView!
+    
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    private var model: SettingsViewModel?
+    private lazy var backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private let contactImage: UIImage? = UIImage(named: "settings_email")?.withRenderingMode(.alwaysTemplate)
+    private let userImage: UIImage? = UIImage(named: "user")?.withRenderingMode(.alwaysTemplate)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let user = User(withName: "Manuel Neuer", andPassword: "WM2018", usingEmail: "manuel.neuer@hm.edu")
+        model = SettingsViewModel(withUser: user)
+        setUpStyling()
+        setUpBinding()
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let backItem = UIBarButtonItem()
-        backItem.title = "Back"
-        navigationItem.backBarButtonItem = backItem // This will show in the next view controller being pushed
+    
+    private func setUpStyling() {
+        userIconImageView.image = userImage
+        userIconImageView.tintColor = UIColor.black.withAlphaComponent(0.8)
+        userIconImageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        emailImageView.image = contactImage
+        emailImageView.tintColor = UIColor.red.withAlphaComponent(0.6)
+        emailImageView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        statsStack.isHidden = true
+        settingStack.isHidden = true
+        pinBackground(backgroundView, to: statsStack)
+        pinBackground(backgroundView, to: settingStack)
+        style(button: logoutButton)
+        
+        guard let user = model?.user else { return }
+        userName.text = user.name
+        userEmail.text = user.email
+        updateCiE()
+        cieCertificateInfo.text = "If you have all 10, you get the CiE Certificate !"
+        updateECTS()
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func updateCiE() {
+        let cieScore = UserStatsService.currentCiE()
+        currentCiE.text = "You currently have \(cieScore) out of 10"
+        currentCiEProgress.progress = Float(cieScore)/10
+    }
+    
+    private func updateECTS() {
+        let ectsScore = UserStatsService.currentECTS()
+        currentECTS.text = "You currently have \(ectsScore) out of 50"
+        currentECTSProgress.progress = Float(ectsScore)/10
+    }
+    
+    private func setUpBinding() {
+        statsButton.addTarget(self, action: #selector(statsTapped), for: .touchUpInside)
+        settingsButton.addTarget(self, action: #selector(settingsTapped), for: .touchUpInside)
+        
+    }
+    
+    private func adjustContentSize() {
+        var contentRect = CGRect.zero
+        for view in mainScrollView.subviews {
+            contentRect = contentRect.union(view.frame)
+        }
+        mainScrollView.contentSize = contentRect.size
+    }
+    
+    @objc
+    func statsTapped() {
+        UIView.animate(withDuration: 0.3) {
+            self.adjustContentSize()
+            self.statsStack.isHidden = !self.statsStack.isHidden
+        }
+    }
+    
+    @objc
+    func settingsTapped() {
+        UIView.animate(withDuration: 0.3) {
+            self.adjustContentSize()
+            self.settingStack.isHidden = !self.settingStack.isHidden
+        }
+    }
+    
+    private func pinBackground(_ view: UIView, to stackView: UIStackView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        stackView.insertSubview(view, at: 0)
+        pin(view: stackView, to: view)
+    }
+    
+    private func pin(view: UIView,to source: UIView) {
+        NSLayoutConstraint.activate([
+            source.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            source.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            source.topAnchor.constraint(equalTo: view.topAnchor),
+            source.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+    }
+    
+    private func style(button btn: UIButton) {
+        btn.backgroundColor = UIColor.red.withAlphaComponent(0.6)
+        btn.layer.cornerRadius = 5
+        btn.layer.borderWidth = 1
+        btn.layer.borderColor = UIColor.black.withAlphaComponent(0.8).cgColor
     }
 }
 
