b681f479ff4a8ea9bfeb9c2bcc1dd8dcc1df1d61
emiorin <miorin@hm.edu>
2018-06-15 10:56:32 +0200

    45-zoom-fullscreen-added

--- CiE-iOS/CiE-iOS/ViewControllers/MapViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/MapViewController.swift
@@ -64,8 +64,26 @@ class MapViewController: UIViewController {
         guard let imageTapped = viewTapped as? UIImageView else { return }
         lastTappedImage = imageTapped
         print("Tapped on \(imageTapped).")
+        
+        let im = tapGestureRecognizer.view as! UIImageView
+        let newImageView = UIImageView(image: lastTappedImage?.image)
+        newImageView.frame = UIScreen.main.bounds
+        newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+        self.navigationController?.isNavigationBarHidden = true
+        self.tabBarController?.tabBar.isHidden = true
     }
     
+    @objc
+    func dismissFullscreenImage(_ sender: UITapGestureRecognizer){
+        self.navigationController?.isNavigationBarHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        sender.view?.removeFromSuperview()
+    }
     
 }
 
