27b9287365044fbfd48d4b836f03a6256cb80f61
Elias Miorin <miorin@hm.edu>
2018-06-21 21:44:27 +0200

    Maps and fullscreen

--- CiE-iOS/CiE-iOS/ViewControllers/MapViewController.swift
+++ CiE-iOS/CiE-iOS/ViewControllers/MapViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
@@ -67,24 +68,102 @@ class MapViewController: UIViewController {
         
         
         
-        let showMap: (UIAlertAction) -> Void = {
+        let goToMap: (UIAlertAction) -> Void = {
             alert in
-            //self.currentDate = self.picker.date
+          
+            if(self.lastTappedImage?.image == self.lothstr.image){
+                let latitude: CLLocationDegrees = 48.154873
+                let longitude: CLLocationDegrees = 11.556105
+                
+                let regionDistance: CLLocationDistance = 1000
+                let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+                let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+                let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+                let placemark = MKPlacemark(coordinate: coordinates)
+                let mapItem = MKMapItem(placemark: placemark)
+                mapItem.name = "University Applied Sciences Munich - Lothstrasse"
+                mapItem.openInMaps(launchOptions: options)
+            }
+            
+            if(self.lastTappedImage?.image == self.karlstr.image){
+                let latitude: CLLocationDegrees = 48.142714
+                let longitude: CLLocationDegrees = 11.568329
+                
+                let regionDistance: CLLocationDistance = 1000
+                let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+                let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+                let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+                let placemark = MKPlacemark(coordinate: coordinates)
+                let mapItem = MKMapItem(placemark: placemark)
+                mapItem.name = "University Applied Sciences Munich - Lothstrasse"
+                mapItem.openInMaps(launchOptions: options)
+            }
+            
+            if(self.lastTappedImage?.image == self.pasing.image){
+                let latitude: CLLocationDegrees = 48.141586
+                let longitude: CLLocationDegrees = 11.451114
+                
+                let regionDistance: CLLocationDistance = 1000
+                let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+                let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+                let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+                let placemark = MKPlacemark(coordinate: coordinates)
+                let mapItem = MKMapItem(placemark: placemark)
+                mapItem.name = "University Applied Sciences Munich - Lothstrasse"
+                mapItem.openInMaps(launchOptions: options)
+            }
+            
+        
+           
+            
+            
+            print("NavigateWithMaps")
         }
-        // setting properties of the datePicker
-        let weekdayLabel = UILabel()
-        weekdayLabel.frame = CGRect(x: 0, y: 15, width: 270, height: 50)
-        //picker.datePickerMode = .date
-        //picker.frame = CGRect(x: 0, y: 15, width: 270, height: 200)
+        let fullscreen: (UIAlertAction) -> Void = {
+            alert in
+            
+            let im = tapGestureRecognizer.view as! UIImageView
+            let newImageView = UIImageView(image: self.lastTappedImage?.image)
+            newImageView.frame = UIScreen.main.bounds
+            newImageView.backgroundColor = .black
+            newImageView.contentMode = .scaleAspectFit
+            newImageView.isUserInteractionEnabled = true
+            let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissFullscreenImage))
+            newImageView.addGestureRecognizer(tap)
+            self.view.addSubview(newImageView)
+            self.navigationController?.isNavigationBarHidden = true
+            self.tabBarController?.tabBar.isHidden = true
+            
+            print("Tschau!")
+        }
+
+        //let weekdayLabel = UILabel()
+        //weekdayLabel.frame = CGRect(x: 0, y: 15, width: 350, height: 350)
+        //weekdayLabel.text = "Hier könnte ihr Bild stehen!"
+        
+        let picture = UIImageView(image: lastTappedImage?.image)
+        picture.frame = CGRect(x: 15, y: 15, width: 240, height: 240)
+        
         let alertController = UIAlertController(title: "\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .alert)
-        //alertController.view.addSubview(picker)
-        //let okAction = UIAlertAction(title: "Ok", style: .default, handler: setDate)
-        okAction.setValue(UIColor.red, forKey: "titleTextColor")
+        
+        var height:NSLayoutConstraint = NSLayoutConstraint(item: alertController.view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: self.view.frame.height * 0.50)
+        alertController.view.addConstraint(height);
+
+        
+        alertController.view.addSubview(picture)
+        
+        let okAction = UIAlertAction(title: "GoToMap", style: .default, handler: goToMap)
+        let cancelAction = UIAlertAction(title: "no gresser", style: .default, handler: fullscreen)
+        
+        okAction.setValue(UIColor.green, forKey: "titleTextColor")
         alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
         present(alertController, animated: true, completion:{})
         
         
     }
+    
+   
     
     @objc
     func dismissFullscreenImage(_ sender: UITapGestureRecognizer){
