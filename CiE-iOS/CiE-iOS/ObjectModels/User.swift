27e2871605bff5d4b5f14d2e1f4323a29869910c
NikitaHans <nikita.s.hans@googlemail.com>
2018-06-22 03:53:22 +0200

    21-Create-UserService.class (#90)
    Implementing working login
    Implementing workaround for backend error

--- CiE-iOS/CiE-iOS/ObjectModels/User.swift
+++ CiE-iOS/CiE-iOS/ObjectModels/User.swift
@@ -7,8 +7,8 @@
 //
 
 protocol UserProtocol {
-    init(withName name: String, andPassword: String, usingEmail: String)
-    var name:String { get }
+    init(withName name: String?, andPassword: String, usingEmail: String)
+    var name:String? { get }
     var password: String { get }
     var isExchange: Bool? { get }
     var departments: Set<Department> { get }
@@ -18,18 +18,26 @@ protocol UserProtocol {
     func becomeExchangeStudent()
     func assignDepartments(_: Set<Department>?)
     func assignDepartments(_: [Department]?)
+    func createLoginRequest() -> [String : String]
     
 }
 
 class User: UserProtocol {
-    public private(set) var name: String
+    func createLoginRequest() -> [String: String] {
+        return [
+            "username" : "\(email)",
+            "password" : "\(password)",
+            ]
+    }
+    
+    public private(set) var name: String?
     public private(set) var password: String
     public private(set) var isExchange: Bool? = false
     public private(set) var email: String
     
     public private(set) var departments: Set<Department> = []
     
-    required init(withName name: String, andPassword password: String, usingEmail email: String) {
+    required init(withName name: String?, andPassword password: String, usingEmail email: String) {
         self.name = name
         self.password = password
         self.email = email
